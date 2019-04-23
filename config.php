ce3a501c4962b5423912f5afd6921dbe9e05f855
NotSoAverageNerd <notsoaveragenerd@gmail.com>
2019-04-23 00:12:06 -0400

    Finished Demo Website's Styling and User Interface

--- config.php
+++ config.php
@@ -1,7 +1,31 @@
 <?php
   //Website Labels:
   $websiteTabLabel = "Raspberry Pi Smart Outlet";
-  $buttonTitleOne =
+  $mainWebsiteLabel = "Not So Average Nerd's Raspberry Pi Smart Outlet";
+  $buttonTitleONE =  "LED ONE";
+  $buttonTitleTWO =  "LED TWO";
+  $buttonTitleTHREE =  "LED THREE";
+  $buttonTitleFOUR =  "LED FOUR";
+  $websiteInformationalParagraph = "Welcome to my very simple web-controller!!! Have fun!!! For more cool projects, Check out my channel: ";
+  $websiteInformationalParagraph2 = "https://www.youtube.com/channel/UCRpLWr2TNSXa7nz5OdYyQHQ?view_as=subscriber";
 
-  //Font settings:
+
+  //Style settings:
+  $websiteBackgroundColor = "#66ccff"; //Make sure you do this in hexcolor format! A nice color picker can be found at https://www.w3schools.com/colors/colors_picker.asp
+
+  //Header styles:
+  $websiteHeaderFontSize = "350%";
+  $websiteHeaderFontColor = "#ffffff";//Make sure you do this in hexcolor format! A nice color picker can be found at https://www.w3schools.com/colors/colors_picker.asp
+  $websiteHeaderFontStyle = "bold";
+  $websiteHeaderWidth = "80%";
+
+  //Paragraph styles:
+  $websiteParagraphFontSize = "130%";
+  $websiteParagraphFontColor = "#ffffff";//Make sure you do this in hexcolor format! A nice color picker can be found at https://www.w3schools.com/colors/colors_picker.asp
+  $websiteParagraphFontStyle = "italic";
+  $websiteParagraphWidth = "75%";
+
+  //Button Styles:
+  $buttonFontSize = "300%";
+  $buttonWidth = "70%";
 ?>
--- index.php
+++ index.php
@@ -9,15 +9,29 @@
     <meta charset="utf-8">
     <title><?php echo $websiteTabLabel ?></title>
   </head>
-  <body>
+  <body style="background-color: <?php echo $websiteBackgroundColor; ?>">
     <center>
-      <button type="button" class="btn btn-primary btn-lg">Light One</button>
+      <h1 style="width: <?php echo $websiteHeaderWidth; ?>; font-size:<?php echo $websiteHeaderFontSize ?>; font-style: <?php echo $websiteHeaderFontStyle ?>; color: <?php echo $websiteHeaderFontColor ?>;"><?php echo $mainWebsiteLabel; ?></h1>
       </br>
-      <button type="button" class="btn btn-primary btn-lg">Light Two</button>
       </br>
-      <button type="button" class="btn btn-primary btn-lg">Light Three</button>
       </br>
-      <button type="button" class="btn btn-primary btn-lg">Light Four</button>
+      <h3 style="width: <?php echo $websiteParagraphWidth; ?>; font-size:<?php echo $websiteParagraphFontSize ?>; font-style: <?php echo $websiteParagraphFontStyle ?>; color: <?php echo $websiteParagraphFontColor ?>;"><?php echo $websiteInformationalParagraph; ?></h3>
+      </br>
+      </br>
+      <h3 style="width: <?php echo $websiteParagraphWidth; ?>; font-size:<?php echo $websiteParagraphFontSize ?>; font-style: <?php echo $websiteParagraphFontStyle ?>; color: <?php echo $websiteParagraphFontColor ?>;"><?php echo $websiteInformationalParagraph2; ?></h3>
+      </br>
+      </br>
+      </br>
+      <button type="button" class="btn btn-primary btn-lg" style="width: <?php echo $buttonWidth; ?>; font-size:<?php echo $buttonFontSize ?>"><?php echo $buttonTitleONE; ?></button>
+      </br>
+      </br>
+      <button type="button" class="btn btn-primary btn-lg" style="width: <?php echo $buttonWidth; ?>; font-size:<?php echo $buttonFontSize ?>"><?php echo $buttonTitleTWO; ?></button>
+      </br>
+      </br>
+      <button type="button" class="btn btn-primary btn-lg" style="width: <?php echo $buttonWidth; ?>; font-size:<?php echo $buttonFontSize ?>"><?php echo $buttonTitleTHREE; ?></button>
+      </br>
+      </br>
+      <button type="button" class="btn btn-primary btn-lg" style="width: <?php echo $buttonWidth; ?>; font-size:<?php echo $buttonFontSize ?>"><?php echo $buttonTitleFOUR; ?></button>
     </center>
   </body>
 </html>
